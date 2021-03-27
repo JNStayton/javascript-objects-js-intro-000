c4505dcb3e516b52f22e73d93dc3a28f00abe83e
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-27 19:00:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {bach: "suite in g"};
 function updatePlaylist (playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle})
 };
+
+function removeFromPlaylist (playlist, artistName) {
+  return delete playlist.artistName;
+}
